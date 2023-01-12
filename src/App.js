4c19ef360b7dddb4675a96ccbccdc37ee6d2f04e
Rubel Hossain99 <rubel820746@gmail.com>
2023-01-12 15:54:03 +0600

    I will created some file for the Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes';
 
 function App() {
+
+
+
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Share/Header/Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <h1 className='bg-black'></h1>
+        </>
+    );
+};
+
+export default Home;
