ea0c337178ec922f32fa889c60d8c24f8a6648a4
taimul <taimul.haque@gmail.com>
2023-01-28 22:21:29 +0600

    team pic added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -7,7 +7,6 @@ import Blog from "../Components/Blog/Blog";
 import Compiler from "../Components/Compiler";
 import SearchAi from "../Components/ChatGptOpenAi/SearchAi";
 import Contact from "../Components/Contact/Contact";
-import CreateShare from "../Components/CreateShare/CreateShare";
 import Home from "../Components/Home/Home";
 import AllFriends from "../Dashboard/AllFriends/AllFriends";
 import Categories from "../Dashboard/Categories/Categories";
@@ -38,7 +37,24 @@ const router = createBrowserRouter([
             {
                 path: '/blog',
                 element: <Blog />
+            },
+            {
+                path: '/compiler',
+                element:<Compiler/>
+            },
+            {
+                path: '/searchai',
+                element: <SearchAi />
+            },
+            {
+                path: '/Register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
+
         ]
     },
     {
